5d1d17d8cd916463039472fc28830806720edd78
MiguelTBG1 <l21390816@chetumal.tecnm.mx>
2025-06-10 14:57:23 -0500

    feat: Agregar rutas para la gestión de indicadores en la API

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\IndicadoresController;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
@@ -10,3 +11,9 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 Route::get('/ping', function () {
     return response()->json(['message' => 'pong']);
 });
+/* RUTAS INDICADORES */
+Route::get('/indicador/getAll', [IndicadoresController::class, 'getAllIndicadores']);
+Route::post('/indicador/insert', [IndicadoresController::class, 'insertIndicador']);
+Route::post('/indicador/upload', [IndicadoresController::class, 'uploadIndicador']);
+Route::delete('/indicador/delete/{id}', [IndicadoresController::class, 'deleteIndicador']);
+Route::post('/indicador/update/{id}', [IndicadoresController::class, 'updateIndicador']);
