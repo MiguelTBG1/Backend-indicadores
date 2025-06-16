c5250a7280cc12836824788686e133e292a773b6
RodrigoAlexander970 <L21390319@chetumal.tecnm.mx>
2025-06-16 14:19:40 -0500

    Se crearon las migraciones para las colecciones base del sistema de usuarios

---
+++ database/migrations/2025_06_16_183450_create_roles_collection.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('roles', function (Blueprint $collection) {
+            $collection->id();
+            $collection->text('nombre');
+            $collection->text('descripcion')->nullable();
+            $collection->json('permisos')->nullable();
+            $collection->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('roles');
+    }
+};
