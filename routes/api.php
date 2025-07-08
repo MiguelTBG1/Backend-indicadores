22a115292593598c48449968f90e98f7eba685fc
RodrigoAlexander970 <L21390319@chetumal.tecnm.mx>
2025-07-08 17:25:47 -0500

    Restructuracion de rutas

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,7 @@ use App\Http\Controllers\ReporteController;
 use App\Http\Controllers\EjesController;
 use App\Http\Controllers\RecursosController;
 use App\Http\Controllers\RolesController;
+use App\Models\Indicadores;
 use Database\Seeders\RolesSeeder;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
@@ -27,39 +28,77 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware(['auth.sanctum'])->group(function () {
 
     /* INDICADORES */
-    Route::get('/indicadores', [IndicadoresController::class, 'index'])->middleware(['abilities:indicadores_leer']);
-    Route::get('/indicadores/{id}', [IndicadoresController::class, 'show'])->middleware(['abilities:indicadores_leer']);
-    Route::post('/indicadores', [IndicadoresController::class, 'store'])->middleware(['abilities:indicadores_crear']);
-    Route::put('/indicadores/{id}', [IndicadoresController::class, 'update'])->middleware(['abilities:indicadores_actualizar']);
-    Route::delete('/indicadores/{id}', [IndicadoresController::class, 'destroy'])->middleware(['abilities:indicadores_eliminar']);
-    Route::put('/indicadores/{id}/configuracion', [IndicadoresController::class, 'updateConfig']);
-    Route::get('/indicadores/{id}/configuracion', [IndicadoresController::class, 'getConfig']);
-    Route::post('/indicadores/filterByDates', [IndicadoresController::class, 'filterByDateRange']);
-    Route::post('/indicadores/upload', [IndicadoresController::class, 'upload'])->middleware(['abilities:indicadores_crear']);
+    Route::controller(IndicadoresController::class)
+        ->prefix('indicadores')
+        ->group(function () {
+            Route::get('/', 'index')->middleware(['abilities:indicadores_leer']);
+            Route::get('{id}', 'show')->middleware(['abilities:indicadores_leer']);
+            Route::post('/', 'store')->middleware(['abilities:indicadores_crear']);
+            Route::put('{id}', 'update')->middleware(['abilities:indicadores_actualizar']);
+            Route::delete('{id}', 'destroy')->middleware(['abilities:indicadores_eliminar']);
+            Route::put('{id}/configuracion', 'updateConfig');
+            Route::get('{id}/configuracion', 'getConfig');
+            Route::post('filterByDates',  'filterByDateRange');
+            Route::post('upload', 'upload')->middleware(['abilities:indicadores_crear']);
+        });
 
-    //PLANTILLAS
-    Route::get('/plantillas', [PlantillaController::class, 'index'])->middleware(['abilities:plantillas_leer']);
-    Route::get('/plantillas/{id}', [PlantillaController::class, 'show'])->middleware(['abilities:plantillas_leer']);
-    Route::post('/plantillas', [PlantillaController::class, 'store'])->middleware(['abilities:plantillas_crear']);
-    Route::put('/plantillas/{id}', [PlantillaController::class, 'update'])->middleware(['abilities:plantillas_actualizar']);
-    Route::delete('/plantillas/{id}', [PlantillaController::class, 'destroy'])->middleware(['abilities:plantillas_borrar']);
-    Route::get('/plantillas/{id}/secciones', [PlantillaController::class, 'getSecciones'])->middleware((['abilities:plantillas_leer']));
+    /* PLANTILLAS */
+    Route::controller(PlantillaController::class)
+        ->prefix('plantillas')
+        ->group(function () {
+            Route::get('/', 'index')->middleware(['abilities:plantillas_leer']);
+            Route::get('{id}', 'show')->middleware(['abilities:plantillas_leer']);
+            Route::post('/',  'store')->middleware(['abilities:plantillas_crear']);
+            Route::put('{id}',  'update')->middleware(['abilities:plantillas_actualizar']);
+            Route::delete('{id}',  'destroy')->middleware(['abilities:plantillas_borrar']);
+            Route::get('{id}/secciones',  'getSecciones')->middleware((['abilities:plantillas_leer']));
+        });
 
-    // DOCUMENTOS
-    Route::get('/documentos/{id}', [DocumentoController::class, 'index'])->where('id', '[a-fA-F0-9]{24}')->middleware(['abilities:documentos_leer']);
-    Route::get('/documentos/{plantillaName}/{documentId}', [DocumentoController::class, 'show'])->middleware(['abilities:documentos_leer']);
-    Route::post('/documentos/{id}', [DocumentoController::class, 'store'])->middleware(['abilities:documentos_crear']);
-    Route::post('/documentos/{plantillaName}/{documentId}', [DocumentoController::class, 'update'])->middleware(['abilities:documentos_actualizar']);
-    Route::delete('/documentos/{plantillaName}/{documentId}', [DocumentoController::class, 'destroy'])->middleware(['abilities:documentos_borrar']);
-    Route::get('/documentos/plantillas', [DocumentoController::class, 'templateNames']);
+    /* DOCUMENTOS */
+    Route::controller(DocumentoController::class)
+        ->prefix('documentos')
+        ->group(function () {
+            Route::get('{id}', 'index')->where('id', '[a-fA-F0-9]{24}')->middleware(['abilities:documentos_leer']);
+            Route::get('{plantillaName}/{documentId}', 'show')->middleware(['abilities:documentos_leer']);
+            Route::post('{id}', 'store')->middleware(['abilities:documentos_crear']);
+            Route::post('{plantillaName}/{documentId}', 'update')->middleware(['abilities:documentos_actualizar']);
+            Route::delete('{plantillaName}/{documentId}', 'destroy')->middleware(['abilities:documentos_borrar']);
+            Route::get('plantillas', 'templateNames');
+        });
 
-    // EJES
-    Route::get('/ejes', [EjesController::class, 'index']);
-    Route::get('/ejes/{id}', [EjesController::class, 'show']);
-    Route::post('/ejes', [EjesController::class, 'store']);
-    Route::put('/ejes/{id}', [EjesController::class, 'update']);
-    Route::delete('/ejes/{id}', [EjesController::class, 'destroy']);
+    /* EJES */
+    Route::controller(EjesController::class)
+        ->prefix('ejes')
+        ->group(function () {
+            Route::get('/', 'index');
+            Route::get('{id}', 'show');
+            Route::post('/', 'store');
+            Route::put('{id}', 'update');
+            Route::delete('{id}', 'destroy');
+        });
 
+    /* RECURSOS */
+    Route::controller(RolesController::class)
+        ->prefix('roles')
+        ->group(function () {
+            Route::get('/', 'index');
+            Route::get('{rolId}', 'show');
+            Route::post('/', 'store');
+            Route::delete('{rolId}', 'destroy');
+            Route::put('{rolId}', 'update');
+        });
+
+
+    /* USUARIOS */
+    Route::controller(UsersController::class)
+        ->prefix('usuarios')
+        ->group(function () {
+            Route::get('/', 'index');
+            Route::get('{id}', 'show');
+            Route::put('{id}', 'update');
+            Route::delete('{id}', 'destroy');
+        });
+    Route::post('/register', [UsersController::class, 'register']);
     // LOGOUT
     Route::post('/logout', [AuthController::class, 'logout']);
 
@@ -69,21 +108,6 @@ Route::middleware(['auth.sanctum'])->group(function () {
 
     /* RECURSOS */
     Route::get('/recursos', [RecursosController::class, 'index']);
-
-    /* ROLES */
-    Route::get('/roles', [RolesController::class, 'index']);
-    Route::get('/roles/{rolId}', [RolesController::class, 'show']);
-    Route::post('/roles', [RolesController::class, 'store']);
-    Route::delete('roles/{rolId}', [RolesController::class, 'destroy']);
-    Route::put('roles/{rolId}', [RolesController::class, 'update']);
-
-    /* USUARIOS */
-    Route::get('/usuarios', [UsersController::class, 'index']);
-    Route::get('/usuarios/{id}', [UsersController::class, 'show']);
-    Route::post('/register', [UsersController::class, 'register']);
-    Route::put('/usuarios/{id}', [UsersController::class, 'update']);
-    Route::delete('/usuarios/{id}', [UsersController::class, 'destroy']);
-
 });
 
 // Reporte PDF
