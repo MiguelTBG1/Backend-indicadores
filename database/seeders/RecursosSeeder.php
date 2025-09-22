12a60dd434570d6a6b7bed64c8ff442bcf1f527a
RodrigoAlexander970 <l21390319@chetumal.tecnm.mx>
2025-09-21 20:42:13 -0500

    Eliminado el archivo FaltantesLogin.md y se mejoró la política de permisos y el constructor de permisos con registros de depuración. Actualizado el seeder de recursos para corregir la clave de un recurso y eliminar recursos innecesarios.

--- database/seeders/RecursosSeeder.php
+++ database/seeders/RecursosSeeder.php
@@ -38,7 +38,7 @@ class RecursosSeeder extends Seeder
                 'tipo' => 'estatico',
             ],
             [
-                'clave' => 'clave_prueba',
+                'clave' => 'Documentos',
                 'nombre' => 'Documentos',
                 'descripcion' => 'Todos los documentos del sistema',
                 'tipo' => 'patron',
@@ -49,45 +49,5 @@ class RecursosSeeder extends Seeder
         foreach ($recursos as $recurso) {
             Recurso::create($recurso);
         }
-
-        // Recurso de plantilla periodos
-        Recurso::create([
-             "clave" => "Periodos_data",
-             "nombre" => "Periodos",
-             "tipo" => "dinamico",
-             "grupo" => "plantillas",
-             "idPlantilla" => "68b0938423ed6ec87508548c",
-             "descripcion" => "Plantilla Periodos",
-        ]);
-
-        // Recurso plantilla Programas Educativos
-        Recurso::create([
-            "clave" => "ProgramaEducativo_data",
-            "nombre" => "Programa Educativo",
-            "tipo" => "dinamico",
-            "grupo" => "plantillas",
-            "idPlantilla" => "68b1df5f34dafa1c910aa02c",
-            "descripcion" => "Plantilla"
-        ]);
-
-        // Recurso plantilla de Profesores
-        Recurso::create([
-            "clave" => "Profesores_data",
-            "nombre" => "Profesores",
-            "tipo" => "dinamico",
-            "grupo" => "plantillas",
-            "idPlantilla" => "68b0a68006688a676a0e6a5d",
-            "descripcion" => "Plantilla profesores"
-        ]);
-
-        // Recurso plantilla de Alumnos
-        Recurso::create([
-            "clave" => "Alumnos_data",
-            "nombre" => "Alumnos",
-            "tipo" => "dinamico",
-            "grupo" => "plantillas",
-            "idPlantilla" => "68bb162223bbc9264e05fca0",
-            "descripcion" => "Plantilla alumnos"
-        ]);
     }
 }
