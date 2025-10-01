64214601a2bfbdbe49b30501044946d27f308485
RodrigoAlexander970 <l21390319@chetumal.tecnm.mx>
2025-10-01 18:23:20 -0500

    Actualizacion de la documentacion Scribe

--- app/Http/Controllers/GraficaController.php
+++ app/Http/Controllers/GraficaController.php
@@ -6,13 +6,31 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use App\Models\Grafica;
 
+/**
+ * @group Gráficas
+ *
+ * Endpoints relacionados con la gestión de gráficas.
+ */
 class GraficaController extends Controller
 {
+    /**
+     * Listar todas las gráficas.
+     *
+     * Retorna una lista de todas las gráficas disponibles.
+     *
+     * */
     public function index()
     {
         return response()->json(['message' => 'GraficaController index method']);
     }
 
+    /**
+     * Mostrar una gráfica específica.
+     *
+     * Retorna la información completa de una gráfica identificada por su ID.
+     *
+     * @urlParam id int requerido El ID de la gráfica que se desea obtener. Ejemplo: 5
+     * */
     public function show($id)
     {
         Log::debug('Buscando grafica con ID: ' . $id);
