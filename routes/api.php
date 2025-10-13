18f7b4e3f039faf759770c8de17e2898854a1ef7
RodrigoAlexander970 <L21390319@chetumal.tecnm.mx>
2025-10-13 14:34:33 -0500

    Rutas faltantes de graficas en

--- routes/api.php
+++ routes/api.php
@@ -123,6 +123,9 @@ Route::middleware(['auth.sanctum'])->group(function () {
     ->group(function () {
         Route::get('/', 'index');
         Route::get('{id}', 'show');
+        Route::post('/', 'store');
+        Route::put('{id}', 'update');
+        Route::delete('{id}', 'destroy');
     });
 });
 
