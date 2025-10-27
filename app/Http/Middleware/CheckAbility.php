2d7156614d8d454fc7b59c4609af08de7ca63f6c
RodrigoAlexander970 <L21390319@chetumal.tecnm.mx>
2025-10-27 14:02:42 -0500

    Mas logs innecesarios borrados

--- app/Http/Middleware/CheckAbility.php
+++ app/Http/Middleware/CheckAbility.php
@@ -23,8 +23,6 @@ class CheckAbility
             return response()->json(['message' => 'No autorizado'], Response::HTTP_FORBIDDEN);
         }
 
-        Log::info("Verificando habilidad: $ability para el usuario: " . $request->user()->id);
-
         // Separamos el recurso del permiso
         [$recurso, $permiso] = explode('.', $ability, 2);
 
@@ -43,14 +41,12 @@ class CheckAbility
         //Reccorremos el arreglo de hablidades permitidas y preguntamos si el token de usuario tiene ese permiso
         foreach ($abilitiesToCheck as $ab) {
             if ($request->user()->tokenCan($ab)) {
-                Log::info("Habilidad encontrada: $ab para el usuario: " . $request->user()->id);
                 $hasAbility = true;
                 break;
             }
         }
 
         if (!$hasAbility) {
-            Log::info("Habilidad no encontrada: $ability para el usuario: " . $request->user()->id);
             return response()->json(['message' => 'No autorizado'], Response::HTTP_FORBIDDEN);
         }
 
--- app/Policies/PlantillaPolicy.php
+++ app/Policies/PlantillaPolicy.php
@@ -22,23 +22,15 @@ class PlantillaPolicy
     public function view(User $user, Plantillas $plantilla): bool
     {
 
-        Log::info("Revisando permiso de lectura para el usuario '{$user->id}' en la plantilla '{$plantilla->_id}'. Habilidad esperada: 'plantilla:{$plantilla->_id}.read'");
-
         // Caso 2: creador puede ver lo suyo
         if ($plantilla->creado_por === $user->_id) {
-            Log::info("El usuario '{$user->_id}' es el creador de la plantilla '{$plantilla->_id}'");
-
             return true;
         }
 
         // Caso 3: Usuarios particulares
         if ($user->currentAccessToken()?->can("plantilla:{$plantilla->_id}.read")) {
-            Log::info("El usuario '{$user->_id}' tiene permiso para ver la plantilla '{$plantilla->_id}'");
-
             return true;
         }
-
-        Log::info("El usuario '{$user->_id}' NO tiene permiso para ver la plantilla '{$plantilla->_id}'");
 
         return false;
     }
@@ -61,20 +53,14 @@ class PlantillaPolicy
      */
     public function update(User $user, Plantillas $plantillas): bool
     {
-        Log::info("Verificando habilidad: plantilla:{$plantillas->_id}.update para el usuario: {$user->_id}  ");
         if ($plantillas->creado_por === $user->_id) {
-            Log::info('El usuario es el creador de la plantilla, puede editarla');
 
             return true;
         }
 
         if ($user->currentAccessToken()?->can("plantilla:{$plantillas->_id}.update")) {
-            Log::info("Habilidad encontrada: plantilla:{$plantillas->_id}.update para el usuario: {$user->_id}  ");
-
             return true;
         }
-
-        Log::info("Habilidad NO encontrada: plantilla:{$plantillas->_id}.update para el usuario: {$user->_id}  ");
 
         return false;
     }
@@ -84,7 +70,6 @@ class PlantillaPolicy
      */
     public function delete(User $user, Plantillas $plantillas): bool
     {
-        Log::info("Verificando habilidad: plantilla:{$plantillas->_id}.delete para el usuario: {$user->_id}  ");
 
         if ($plantillas->creado_por === $user->_id) {
             return true;
@@ -93,8 +78,6 @@ class PlantillaPolicy
         if ($user->currentAccessToken()?->can("plantilla:{$plantillas->_id}.delete")) {
             return true;
         }
-
-        Log::info('El usuario NO tiene permiso para eliminar la plantilla');
 
         return false;
     }
